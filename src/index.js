6e1b82add5ed1d5441130a80e736934fedb28362
zhuozhongcao <czz5242199@gmail.com>
2020-09-19 22:38:38 +0800

    support script

--- src/index.js
+++ src/index.js
@@ -1,34 +1,37 @@
 import Clipboard from "clipboard";
 
-/**
- * @param {string} text
- */
-function copy(text) {
-  return new Promise(function (resolve, reject) {
-    const fakeBtn = document.createElement("button");
-    const clipboard = new Clipboard(fakeBtn, {
-      text: () => text,
-      action: () => "copy",
-      container: document.body,
+((global, name, factory) => {
+  if (typeof exports === "object" && typeof module !== "undefined") {
+    module.exports = factory();
+  } else if (typeof define === "function" && (define.amd || define.cmd)) {
+    define(factory);
+  } else {
+    global[name] = factory.apply(this);
+  }
+})(
+  this,
+  "oneLineCopy",
+  /**
+   * @param {string} text
+   */ function copy(text) {
+    return new Promise(function (resolve, reject) {
+      const fakeBtn = document.createElement("button");
+      const clipboard = new Clipboard(fakeBtn, {
+        text: () => text,
+        action: () => "copy",
+        container: document.body,
+      });
+      clipboard.on("success", function (e) {
+        clipboard.destroy();
+        resolve(e);
+      });
+      clipboard.on("error", function (e) {
+        clipboard.destroy();
+        reject(e);
+      });
+      document.body.appendChild(fakeBtn);
+      fakeBtn.click();
+      document.body.removeChild(fakeBtn);
     });
-    clipboard.on("success", function (e) {
-      clipboard.destroy();
-      resolve(e);
-    });
-    clipboard.on("error", function (e) {
-      clipboard.destroy();
-      reject(e);
-    });
-    document.body.appendChild(fakeBtn);
-    fakeBtn.click();
-    document.body.removeChild(fakeBtn);
-  });
-}
-
-if (typeof define === "function" && define.amd) {
-  define([], copy);
-} else if (typeof exports === "object") {
-  module.exports = copy;
-} else {
-  root.copy = copy;
-}
+  }
+);
